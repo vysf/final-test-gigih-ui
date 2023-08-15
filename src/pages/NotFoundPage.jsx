76b7f60670ba1a57ba1205aa90cd6877957eb1f5
vysf <yusufumar@student.untan.ac.id>
2023-08-16 02:39:54 +0700

    perbaikan sedikti

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -14,6 +14,7 @@ function NotFoundPage() {
         Ayo segera kembali ke
         {' '}
         <Link to="/">rumah</Link>
+        {' '}
         sebelum hari menjadi gelap.
       </p>
     </div>
